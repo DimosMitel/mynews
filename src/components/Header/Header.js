9ec7913db3e15c82ffa3009ae30996e71f87e1d8
Dimos Mytel <dimos.mitel@gmail.com>
2020-10-31 20:29:42 +0200

    Add Header Component with navigation to root, Sign Up and Login page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,36 @@
 import React from "react"
 import './Header.css';
-
+import {
+    Link,
+  } from "react-router-dom";
 
 const Header = () => {
     return (
+    
 
       <div className="container-header">
-         <p>Header</p>
+          <div className="item1"><Link to="/">Mynews</Link></div>
+
+          <div className="item2">      
+            <div>
+              <nav>
+                 <Link to="/Login">Login</Link>
+              </nav>
+             
+            </div>
+    
+          </div>
+
+          <div className="item3">      
+            <div>
+         
+              <nav>
+                 <Link to="/signup">SignUp</Link>
+              </nav>
+            </div>
+    
+          </div>
+                
       </div>
 
     )
