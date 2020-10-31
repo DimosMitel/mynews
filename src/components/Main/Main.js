8370798858fe71420d2cb4269028e9b5f1f61229
Dimos Mytel <dimos.mitel@gmail.com>
2020-10-31 20:32:21 +0200

    Add react-router and basic routes for signup, login and root

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,22 @@
 import React from "react"
 import './Main.css'
+import {
+    Switch,
+    Route,
+  } from "react-router-dom";
+
+  import LoginControl from '../../features/Login/LoginControl'
+  import SignUp from '../../features/SignUp/SignUp'  
 
 const Main = () => {
     return (
-
-    <div><p>Main</p></div>
+    <>    
+        <Switch>
+          
+          <Route exact path="/login" component={LoginControl} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+    </>   
 
     )
 }
